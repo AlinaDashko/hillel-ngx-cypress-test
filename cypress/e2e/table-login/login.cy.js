8805fee36492923e6d013b9abc42a120e5bd0b8a
adashko <dashkoalina@yahoo.com>
2023-10-23 23:59:58 +0300

    added edits to the homework assignment 13. Cypress actions

--- cypress/e2e/table-login/login.cy.js
+++ cypress/e2e/table-login/login.cy.js
@@ -1,22 +1,10 @@
+import {LoginPage} from "../../PageObjects/LoginPage";
+
 describe("Login page", () => {
   it('The form must be filled in correctly', () => {
-    cy.visit('/auth/login');
-
-    cy.get('nb-login').as("loginForm");
-    cy.get("@loginForm").should('be.visible');
-
-    cy.get('#input-email').as("emailAddressInput");
-    cy.get("@emailAddressInput").should('be.visible').type("alina@test.test", { delay: 50 });
-
-    cy.get('#input-password').as("passwordInput");
-    cy.get("@passwordInput").should('be.visible').type("Password1+", { delay: 50 });
-
-    cy.get('nb-checkbox[name="rememberMe"]').find('span.custom-checkbox').as("checkbox");
-    cy.get("@checkbox").should('be.visible').click();
-
-    cy.get('button[fullwidth][status="primary"]').as("loginButton");
-    cy.get("@loginButton").click();
-
-    cy.url().should('include', '/pages');
-  });
-});
+    const loginPage = new LoginPage()
+    loginPage.navigate()
+    loginPage.fill("alina@test.test", "Password1+", true)
+    loginPage.clickLoginButton()
+  })
+})
